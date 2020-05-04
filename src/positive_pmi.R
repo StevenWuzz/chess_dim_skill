21e4b8b7be748092136af20d505d7d1ae6e2d940
StevenWuzz <40755531+StevenWuzz@users.noreply.github.com>
2020-05-04 03:20:43 -0400

    Create positive_pmi.R

---
+++ src/positive_pmi.R
@@ -0,0 +1,25 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(tidyr)
+library(reshape)
+library(schoolmath)
+
+chess_data <- fread("/w/225/1/chess/tactics/glicko_user_tactics_problem.csv_00",data.table = FALSE,
+                    showProgress = TRUE)
+
+passed = subset(chess_data,is_passed == 1)
+train_dat = select(passed,tactics_problem_id,user_hash)
+vocab_dat = train_dat %>% group_by(tactics_problem_id) %>% summarise(count = n()) %>% ungroup()
+context_dat =  train_dat %>% group_by(user_hash) %>% summarise(count = n()) %>% ungroup()
+
+trans_data <- train_dat %>% group_by(tactics_problem_id, user_hash) %>% mutate(count= n()) #everything is 1???
+trans_data_vocab <- train_dat %>% group_by(tactics_problem_id) %>% mutate(count_vocab = n())
+trans_data_context <- train_dat %>% group_by(user_hash) %>% mutate(count_context = n())
+cardinality_D <- sum(vocab_dat$count) + sum(context_dat$count)
+vec_cardinality_D <- rep(cardinality_D, nrow(trans_data))
+cardinality_D_data <- data.frame("car_D" = vec_cardinality_D)
+pmi_data <- log(trans_data$count) + log(cardinality_D_data$car_D) - log(trans_data_vocab$count_vocab) - log(trans_data_context$count_context)
+pmi_data_frame <- data.frame("PMI_value" = pmi_data)
+positive_pmi <- subset(pmi_data_frame, pmi_data_frame$PMI_value >= 0)
